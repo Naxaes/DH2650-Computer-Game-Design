a0cc43c55e3080cf58b25ae832dfb29bf7bbcb28
Karl Brunnberg <karlviktoroesten@gmail.com>
2022-04-05 22:08:05 +0200

    Added component that makes enemy shoot projectile in direction of movement

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -3,48 +3,99 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
-{   
+{
     /*
      * - Public Variables -
-     * transform: the Transform component of the enemy that should move.
-     * isMoving: set to true if the enemy should move, false if the enemy should stay still.
-     * range: the range of the enemy, i.e. the distance it should move before changing direction.
+     * transform: the Transform component of the enemy.
+     * playerTransform: the Transform component of the player or other character that this enemy should chase.
+     * shouldChase: set to true if the enemy should chase the player, false if it should move back and forth only.
+     * roamingRange: the standard movement range of the enemy, i.e. the distance it should roam before changing direction.
+     * visionRange: the vision/sight range of the enemy. It determines how near the player has to be before the enemy will chase it.
      * speed: the speed at which the enemy should move.
      */
     public Transform transform;
-    public bool isMoving = true;
-    public float range = 7.0f;
-    public float speed = 0.005f;
+    public Transform playerTransform;
+    public bool shouldChase = true;
+    public float roamingRange = 7.0f;
+    public float visionRange = 3.0f;
+    public float speed = 2.0f;
+    public int direction = 1;
 
     float counter;
-    int direction;
+    float distance;
+    bool isChasing;
+    Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
     {   
+        // Does not chase at start
+        isChasing = false;
+
         // Reset counter
         counter = 0.0f;
-
-        // Reset direction
-        direction = 1;
     }
 
     // Update is called once per frame
     void Update()
+    {   
+        // Calculate the direction from enemy to player
+        Vector2 dir = playerTransform.position - transform.position;
+        dir.Normalize();
+        movement = dir;
+
+        // Calculate the horizontal distance between enemy and player
+        distance = Mathf.Abs(transform.position.x - playerTransform.position.x);
+        if (shouldChase && distance <= visionRange)
+        {   
+            // Go chase
+            isChasing = true;
+        } else
+        {   
+            // Don't chase
+            isChasing = false;
+        }
+    }
+
+    void FixedUpdate()
     {
-        if (isMoving)
+        if (isChasing)
         {
-            if (counter <= range)
+            chasePlayer(movement);
+        }
+        else
+        {   
+            // Check that we haven't roamed too far
+            if (counter <= roamingRange)
             {
-                transform.position = new Vector2(transform.position.x - speed * direction, transform.position.y);
+                transform.position = new Vector2(transform.position.x - (direction * speed), transform.position.y);
+                // Increase counter with distance traveled
                 counter += speed;
             }
-
-            if (counter > range)
+            // If we have roamed too far, flip and move the other way
+            if (counter > roamingRange)
             {
-                direction *= -1;
-                counter = 0.0f;
+                Flip();
             }
         }
+    }
+
+    void chasePlayer(Vector2 playerDirection)
+    {
+        transform.position = new Vector2(transform.position.x + (playerDirection * speed).x, transform.position.y);
+        if (transform.position.x > playerTransform.position.x)
+        {
+            direction = 1;
+        }
+        else
+        {
+            direction = -1;
+        }
+    }
+
+    void Flip()
+    {
+        direction *= -1;
+        counter = 0.0f;
     }
 }
--- Assets/MoveCharacter.cs
+++ Assets/MoveCharacter.cs
@@ -6,26 +6,20 @@ public class MoveCharacter : MonoBehaviour
 {
     public Transform transform;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position = new Vector2(transform.position.x - 0.04f, transform.position.y);
+            transform.position = new Vector2(transform.position.x - 0.01f, transform.position.y);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position = new Vector2(transform.position.x + 0.04f, transform.position.y);
+            transform.position = new Vector2(transform.position.x + 0.01f, transform.position.y);
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position = new Vector2(transform.position.x, transform.position.y + 0.04f);
+            transform.position = new Vector2(transform.position.x, transform.position.y + 0.03f);
         }
     }
 }
